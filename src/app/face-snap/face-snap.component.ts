421d2681ef23d8432ff1914beda94d3febd1f83b
StephanieVester <stephanie.vester.fr@gmail.com>
2023-01-25 11:11:08 +0100

    event binding done

--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -1,11 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-face-snap',
   templateUrl: './face-snap.component.html',
   styleUrls: ['./face-snap.component.scss']
 })
-export class FaceSnapComponent {
-
+export class FaceSnapComponent implements OnInit {
+  title!: string;
+  description!: string;
+  createdDate!: Date;
+  snaps!: number;
+  imageUrl!: string;
+  snapBtnTxt!: string;
+  isSnapped!: boolean;
   
+  ngOnInit() {
+    this.title = 'Archibald';
+    this.description = 'Un ami de toujours';
+    this.createdDate = new Date();
+    this.snaps = 5;
+    this.imageUrl = 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg';
+    this.snapBtnTxt = 'Add Snap!';
+    this.isSnapped = false;
+  }
+
+  onClickSnapButton (){
+    if(this.isSnapped === false){
+      this.snaps++;
+      this.isSnapped = true;
+      this.snapBtnTxt = 'Snapped! Remove Snap';
+    } else {
+      this.snaps--;
+      this.isSnapped = false;
+      this.snapBtnTxt = 'Add Snap!';
+    }
+
+  }
+
 }
